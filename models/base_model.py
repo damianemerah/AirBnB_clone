72e12b8daa366c9c98ac822993c126563706978f
dammy995 <damianemerah1@gmail.com>
2023-02-12 13:13:33 -0800

    All task

--- models/base_model.py
+++ models/base_model.py
@@ -3,43 +3,54 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
 
     """Class from which all other classes will inherit"""
 
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
-
     def __init__(self, *args, **kwargs):
-        """Initialise instance attributes
+        """Initializes instance attributes
 
         Args:
             - *args: list of arguments
-            - **kwargs: dict of key-value arguments
+            - **kwargs: dict of key-values arguments
         """
-        if kwargs is not NONE and Kwarg != {}:
-            return
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        """Return official string representation"""
+        """Returns official string representation"""
 
-        return "[{}] ({}) {}"\
-                .format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates the public instance attribute updated_at"""
+        """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all Key/values"""
+        """returns a dictionary containing all keys/values of __dict__"""
 
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isodormat()
-
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         return my_dict
